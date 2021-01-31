2285fc1ee4d7819fa08342fb62af2b503cf06f7e
chyppoli <claude.hyppolite@gmail.com>
2021-01-31 15:41:54 -0500

    remove contact page route. add route for gh-pages homepage

--- src/App.js
+++ src/App.js
@@ -7,23 +7,17 @@ import Projects from "./containers/Projects.js";
 import Contact from "./containers/Contact";
 import Footer from "./components/Footer";
 
-
 function App() {
   return (
     <>
       <Router>
-          <Navbar />
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/projects" component={Projects} />
-          <Route exact path="/contact" component={Contact} />
-          {/* <Switch>
-  17              <Route exact path="/portfolio-react-remix" component={Home} />
-
-  21              <Route component={Home} />
-  22:           </Switch> */}
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/portfolio-react-remix" component={Home} />
       </Router>
-        <Footer />
+      <Footer />
     </>
   );
 }
