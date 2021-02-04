0e25f7e7a916e1c5c4f96b950127c804fc7f8508
chyppoli <claude.hyppolite@gmail.com>
2021-02-03 19:27:52 -0500

    add separate projectcard components for each card to get images to show

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import ProjectCardWide from '../components/ProjectCardWide'
+// import ProjectCardWide from '../components/ProjectCardWide'
+import ProjectCard1 from '../components/ProjectCard1'
+import ProjectCard2 from '../components/ProjectCard2'
+import ProjectCard3 from '../components/ProjectCard3'
+import ProjectCard4 from '../components/ProjectCard4'
+import ProjectCard5 from '../components/ProjectCard5'
+import ProjectCard6 from '../components/ProjectCard6'
 
 const Projects = () => {
     return (
         <main className="container">
-                {/* <ProjectCard /> */}
-                <ProjectCardWide />
+                <ProjectCard1 />
+                <ProjectCard2 />
+                <ProjectCard3 />
+                <ProjectCard4 />
+                <ProjectCard5 />
+                <ProjectCard6 />
            
         </main>
     );
